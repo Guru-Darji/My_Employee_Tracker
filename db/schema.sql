31314496952ee17aac13386096cca7bdb3d8f4b8
Guru Darji <gurudarji@ymail.com>
2022-03-29 19:01:48 -0400

    made the role

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE department (
     name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
